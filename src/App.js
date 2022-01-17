aeefa68c6e2458cb0b6454c80ddd5ba9f7437c99
Nattakorn Kompanich <perth.ploy@gmail.com>
2022-01-17 15:42:03 +0700

    add subpages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { Home, Navbar } from "./components";
+import { Home, Navbar, Products, Services, SignUp } from "./components";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
@@ -9,6 +9,9 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/" exact element={<Home />} />
+          <Route path="/products" exact element={<Products />} />
+          <Route path="/service" exact element={<Services />} />
+          <Route path="/signup" exact element={<SignUp />} />
         </Routes>
       </Router>
     </div>
--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,5 @@
 export { default as Navbar } from "./Navbar/Navbar";
 export { default as Home } from "./pages/Home";
+export { default as Products } from "./pages/Products";
+export { default as Services } from "./pages/Services";
+export { default as SignUp } from "./pages/SignUp";
