c3cbff1b7cd8aa10b338ac2c36c678c4f61d1d08
Nattakorn Kompanich <perth.ploy@gmail.com>
2022-01-17 13:10:31 +0700

    create Cards

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import Cards from '../Card/Cards'
 import HeroSection from '../HeroSection/HeroSection'
 
 function Home() {
   return (
     <>
       <HeroSection />
+      <Cards />
     </>
   )
 }
